134b26c8f4786935cdd7c5cebbbea3e2d6b00f98
jwenture <john@jwenture.com>
2020-09-24 00:50:37 +0800

    hostname

--- app/home/routes.py
+++ app/home/routes.py
@@ -13,7 +13,7 @@ from jinja2 import TemplateNotFound
 #@login_required
 def index():
     print("AAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHH")
-    return render_template('index.html', segment='index')
+    return render_template('index.html', segment='index', hostname=request.remote_addr)
 
 @blueprint.route('/<template>')
 #@login_required
